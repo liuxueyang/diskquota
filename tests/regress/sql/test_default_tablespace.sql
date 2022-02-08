4b0baf013eba84085d6fcf098b405154856cee64
liuxueyang <liuxueyang457@gmail.com>
2022-02-08 18:52:33 +0800

    set tablespaceoid to MyDatabaseTableSpace if it's not OidIsValid
    Because we need the real tablespace id to get table size or add it to blackmap.

---
+++ tests/regress/sql/test_default_tablespace.sql
@@ -0,0 +1,38 @@
+-- Test default tablespace
+-- start_ignore
+\! mkdir -p /tmp/custom_tablespace
+-- end_ignore
+
+DROP TABLE if EXISTS t;
+CREATE TABLE t (i int);
+SELECT diskquota.set_role_tablespace_quota('gpadmin', 'pg_default', '1 MB');
+SELECT diskquota.wait_for_worker_new_epoch();
+-- expect insert to fail
+INSERT INTO t SELECT generate_series(1, 1000000);
+DROP TABLE IF EXISTS t;
+
+-- database in customized tablespace with hard limits on
+CREATE TABLESPACE custom_tablespace LOCATION '/tmp/custom_tablespace';
+CREATE DATABASE db_with_tablespace TABLESPACE custom_tablespace;
+\c db_with_tablespace;
+CREATE EXTENSION diskquota;
+SELECT diskquota.enable_hardlimit();
+
+SELECT diskquota.set_role_tablespace_quota(current_role, 'custom_tablespace', '1MB');
+SELECT diskquota.wait_for_worker_new_epoch();
+-- expect create table to fail
+CREATE TABLE t_in_custom_tablespace AS SELECT generate_series(1, 1000000);
+
+-- expect create table to succeed
+SELECT diskquota.wait_for_worker_new_epoch();
+CREATE TABLE t_in_custom_tablespace AS SELECT generate_series(1, 100);
+-- expect insert records to fail
+INSERT INTO t_in_custom_tablespace SELECT generate_series(1, 1000000);
+
+-- clean up
+DROP EXTENSION diskquota;
+DROP TABLE IF EXISTS t_in_custom_tablespace;
+\c postgres;
+DROP DATABASE IF EXISTS db_with_tablespace;
+DROP TABLESPACE IF EXISTS custom_tablespace;
+\! rm -rf /tmp/custom_tablespace
