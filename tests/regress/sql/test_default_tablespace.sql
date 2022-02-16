9de9591ec7de2ed037fff625cdf91a54fd632911
liuxueyang <liuxueyang457@gmail.com>
2022-02-16 19:14:01 +0800

    add soft limits test in test_default_tablespace

--- tests/regress/sql/test_default_tablespace.sql
+++ tests/regress/sql/test_default_tablespace.sql
@@ -5,8 +5,6 @@
 \! mkdir -p /tmp/custom_tablespace
 -- end_ignore
 
-SELECT diskquota.enable_hardlimit();
-
 DROP ROLE if EXISTS role1;
 CREATE ROLE role1 SUPERUSER;
 SET ROLE role1;
@@ -14,29 +12,56 @@ SET ROLE role1;
 DROP TABLE if EXISTS t;
 CREATE TABLE t (i int);
 
+-- with hard limits off
+SELECT diskquota.disable_hardlimit();
 SELECT diskquota.set_role_tablespace_quota('role1', 'pg_default', '1 MB');
+SELECT diskquota.wait_for_worker_new_epoch();
+-- expect insert to success
+INSERT INTO t SELECT generate_series(1, 100);
+INSERT INTO t SELECT generate_series(1, 1000000);
+-- expect insert to fail
+INSERT INTO t SELECT generate_series(1, 1000000);
+
+DROP TABLE IF EXISTS t;
+CREATE TABLE t (i int);
+
+-- with hard limits on
+SELECT diskquota.enable_hardlimit();
 SELECT diskquota.wait_for_worker_new_epoch();
 -- expect insert to fail
 INSERT INTO t SELECT generate_series(1, 1000000);
 DROP TABLE IF EXISTS t;
 
--- database in customized tablespace with hard limits on
+-- database in customized tablespace
 CREATE TABLESPACE custom_tablespace LOCATION '/tmp/custom_tablespace';
 CREATE DATABASE db_with_tablespace TABLESPACE custom_tablespace;
 \c db_with_tablespace;
 SET ROLE role1;
 CREATE EXTENSION diskquota;
-SELECT diskquota.enable_hardlimit();
 
+-- with hard limits off
+SELECT diskquota.disable_hardlimit();
 SELECT diskquota.set_role_tablespace_quota('role1', 'custom_tablespace', '1 MB');
 SELECT diskquota.wait_for_worker_new_epoch();
-
+-- expect insert to success
+CREATE TABLE t_in_custom_tablespace AS SELECT generate_series(1, 100);
+INSERT INTO t_in_custom_tablespace SELECT generate_series(1, 1000000);
 -- start_ignore
 SELECT * from diskquota.blackmap;
 -- end_ignore
+-- expect insert to fail
+INSERT INTO t_in_custom_tablespace SELECT generate_series(1, 1000000);
 
+-- with hard limits on
+SELECT diskquota.enable_hardlimit();
+SELECT diskquota.wait_for_worker_new_epoch();
+
+DROP TABLE IF EXISTS t_in_custom_tablespace;
 -- expect create table to fail
 CREATE TABLE t_in_custom_tablespace AS SELECT generate_series(1, 1000000);
+-- start_ignore
+SELECT * from diskquota.blackmap;
+-- end_ignore
 
 -- clean up
 DROP TABLE IF EXISTS t_in_custom_tablespace;
