50f167db5082d9cd4d8e4f0d8787db584b54a224
liuxueyang <liuxueyang457@gmail.com>
2022-03-28 10:26:54 +0800

    add BeforeElse conf to .clang-format

--- diskquota.c
+++ diskquota.c
@@ -42,12 +42,9 @@ PG_MODULE_MAGIC;
 #define DISKQUOTA_APPLICATION_NAME "gp_reserved_gpdiskquota"
 
 /* clang-format off */
-#if !defined(DISKQUOTA_VERSION) || \
-	!defined(DISKQUOTA_MAJOR_VERSION) || \
-	!defined(DISKQUOTA_PATCH_VERSION) || \
-	!defined(DISKQUOTA_MINOR_VERSION) || \
-	!defined(DISKQUOTA_BINARY_NAME)
-	#error Version not found. Please check if the VERSION file exists.
+#if !defined(DISKQUOTA_VERSION) || !defined(DISKQUOTA_MAJOR_VERSION) || !defined(DISKQUOTA_PATCH_VERSION) || \
+    !defined(DISKQUOTA_MINOR_VERSION) || !defined(DISKQUOTA_BINARY_NAME)
+#error Version not found. Please check if the VERSION file exists.
 #endif
 /* clang-format on */
 
@@ -130,7 +127,8 @@ _PG_init(void)
 	{
 		ereport(ERROR, (errmsg("[diskquota] booting " DISKQUOTA_VERSION ", but " DISKQUOTA_BINARY_NAME
 		                       " not in shared_preload_libraries. abort.")));
-	} else
+	}
+	else
 	{
 		ereport(INFO, (errmsg("booting diskquota-" DISKQUOTA_VERSION)));
 	}
@@ -298,7 +296,8 @@ disk_quota_worker_main(Datum main_arg)
 			snprintf(_errmsg, sizeof(_errmsg), _errfmt, times * diskquota_naptime);
 
 			init_ps_display("bgworker:", "[diskquota]", dbname, _errmsg);
-		} else
+		}
+		else
 		{
 			init_ps_display("bgworker:", "[diskquota]", dbname,
 			                "v" DISKQUOTA_VERSION " is not matching with current SQL. stop working");
