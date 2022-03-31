968c67f45f8b62756bb1a6c50bf005c4385b98a4
liuxueyang <liuxueyang457@gmail.com>
2022-03-31 22:29:11 +0800

    create table without partition

--- tests/stress/init_table_size.py
+++ tests/stress/init_table_size.py
@@ -6,14 +6,28 @@ def run(db: str, num_tables: int, num_rows_per_table: int, enable_diskquota: boo
     db_clean(db)
     if enable_diskquota:
         db_enable_diskquota(db)
+
+    create_tables_without_partition(db, num_tables, num_rows_per_table)
+    db_exec(db, 'SELECT diskquota.init_table_size_table();')
+
+def create_tables_with_partition(db: str, num_tables: int, num_rows_per_table: int):
     db_exec(db, f'''
         CREATE TABLE t1 (pk int, val int)
         DISTRIBUTED BY (pk)
         PARTITION BY RANGE (pk) (START (1) END ({num_tables}) INCLUSIVE EVERY (1));
     ''')
     db_exec(db, f'''
-        INSERT INTO t1 
+        INSERT INTO t1
         SELECT pk, val
         FROM generate_series(1, {num_rows_per_table}) AS val, generate_series(1, {num_tables}) AS pk;
     ''')
-    db_exec(db, 'SELECT diskquota.init_table_size_table();')
+
+def create_tables_without_partition(db: str, num_tables: int, num_rows_per_table: int):
+    for i in range(num_tables):
+        db_exec(db, f'''
+            CREATE TABLE table_{i} (i int) DISTRIBUTED BY (i);
+        ''')
+        db_exec(db, f'''
+            INSERT INTO table_{i}
+            SELECT generate_series(1, {num_rows_per_table});
+        ''')
